f767c5d37464de6f16bf79e08b53cb99489667f4
mattlindsey <matthew.r.lindsey@gmail.com>
2013-11-24 19:14:18 -0500

    added validations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-/* Author:
+/* Author: MRL
 
 */
+
+$(document).ready(function(){
+  $('#signup form').validate({
+    rules: {
+      name: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      website: {
+        url: true
+      },
+      password: {
+        minlength: 6,
+        required: true
+      },
+      passconf: {
+        equalTo: "#password"
+      }
+    },
+    success: function(label) {
+      label.text('OK!').addClass('valid');
+    }
+  });
+});
 
 
 
